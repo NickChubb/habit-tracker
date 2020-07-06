1ca598f8883837a2733ddf1a58b693516b5f76a1
Nick Chubb <Nick.r.chubb@gmail.com>
2020-07-06 10:19:09 -0700

    Adding list to skills screen

--- app/screens/skills-screen/skills-screen.tsx
+++ app/screens/skills-screen/skills-screen.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent as Component } from "react"
-import { View, Image, ViewStyle, TextStyle, ImageStyle, SafeAreaView } from "react-native"
+import { View, Image, ViewStyle, TextStyle, ImageStyle, SafeAreaView, FlatList} from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { observer } from "mobx-react-lite"
@@ -10,13 +10,24 @@ import { color, spacing, typography, topbar } from "../../theme"
 const FULL: ViewStyle = { flex: 1 }
 
 // SKILLS is the object of the users experience in each life skill
-const SKILLS = {
-    mindfulness: 0,
-    hygiene: 0,
-    cleaning: 0,
-    cooking: 0,
-    exercise: 0
-}
+const SKILLS = [
+    {
+        name: "Cleaning",
+        experience: 0
+    },
+    {
+        name: "Cooking",
+        experience: 0
+    },
+    {
+        name: "Mindfulness",
+        experience: 0
+    },
+    {
+        name: "Hygiene",
+        experience: 0
+    }
+]
 
 export const SkillsScreen: Component = observer(function SkillsScreen() {
     const navigation = useNavigation();
@@ -26,6 +37,11 @@ export const SkillsScreen: Component = observer(function SkillsScreen() {
             
             <Text style={topbar}>Skills</Text>
             
+            <FlatList 
+                data={SKILLS}
+                renderItem={({item}) => <Text>{SKILLS.name}</Text>}
+            />
+                
 
         </View>
     );
