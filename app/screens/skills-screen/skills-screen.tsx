7f9e7006b17d5585d8065c2aba282dcbd51a64f6
Nick Chubb <Nick.r.chubb@gmail.com>
2020-07-06 12:09:41 -0700

    Add progress bar and styling

--- app/screens/skills-screen/skills-screen.tsx
+++ app/screens/skills-screen/skills-screen.tsx
@@ -5,29 +5,69 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { observer } from "mobx-react-lite"
 import { Button, Header, Screen, Text, Wallpaper } from "../../components"
 import { color, spacing, typography, topbar } from "../../theme"
+import { iteratorSymbol } from "mobx/lib/internal"
+import * as Progress from 'react-native-progress'
 
 
 const FULL: ViewStyle = { flex: 1 }
+const LIST_ITEM: ViewStyle = {
+    backgroundColor: "#fff",
+    fontWeight: "bold",
+    marginTop: 5,
+    padding: 20,
+    textAlign: "center"
+}
+const LIST_TITLE: ViewStyle = {
+    color: "#555555",
+    fontSize: 18,
+    fontWeight: "bold"
+}
+const LIST_LEVEL: ViewStyle = {
+    color: "#555555"
+}
 
 // SKILLS is the object of the users experience in each life skill
 const SKILLS = [
     {
+        id: 0,
         name: "Cleaning",
-        experience: 0
+        experience: 620
     },
     {
+        id: 1,
         name: "Cooking",
-        experience: 0
+        experience: 100
     },
     {
+        id: 2,
         name: "Mindfulness",
-        experience: 0
+        experience: 350
     },
     {
+        id: 3,
         name: "Hygiene",
-        experience: 0
+        experience: 700
     }
 ]
+
+let levelCalculator = (exp) => {
+    return exp / 1000;
+}
+
+function Skill({skill}){
+    return(
+        <View style={LIST_ITEM}>
+            <View style={{flexDirection: "row", margin: 3}}>
+                <Text style={LIST_TITLE}>{skill.name}</Text>
+                <Text style={LIST_LEVEL}>{levelCalculator(skill.experience)}</Text>
+            </View>
+            <View style={{flexDirection: "row", margin: 3}}>
+                <Text style={{color: "black"}}>{skill.experience} / 1000</Text>
+                <Progress.Bar progress={skill.experience / 1000} height={16} width={250}/>
+            </View>
+        </View>
+    );
+}
 
 export const SkillsScreen: Component = observer(function SkillsScreen() {
     const navigation = useNavigation();
@@ -39,7 +79,7 @@ export const SkillsScreen: Component = observer(function SkillsScreen() {
             
             <FlatList 
                 data={SKILLS}
-                renderItem={({item}) => <Text>{SKILLS.name}</Text>}
+                renderItem={({item}) => <Skill skill={item} />}
             />
                 
 
