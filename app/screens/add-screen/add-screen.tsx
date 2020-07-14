9cbb42087a0ef922a1f81eba5b1ba59c5c7507cf
Nick Chubb <Nick.r.chubb@gmail.com>
2020-07-14 10:25:51 -0700

    More add elements styling

--- app/screens/add-screen/add-screen.tsx
+++ app/screens/add-screen/add-screen.tsx
@@ -1,12 +1,12 @@
 import React, { FunctionComponent as Component } from "react"
-import { View, Image, ViewStyle, TextStyle, ImageStyle, SafeAreaView, TextInput  } from "react-native"
+import { View, Image, ViewStyle, TextStyle, ImageStyle, SafeAreaView, TextInput, StyleSheet  } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { observer } from "mobx-react-lite"
 import { Button, Header, Screen, Text, Wallpaper } from "../../components"
 import { color, spacing, typography, topbar } from "../../theme"
 import { T } from "ramda"
-
+import RNPickerSelect from 'react-native-picker-select';
 
 const FULL: ViewStyle = { 
     flex: 1
@@ -24,6 +24,58 @@ const INPUT: ViewStyle = {
     padding: 8,
     fontSize: 16
 }
+const ADD_BUTTON: ViewStyle = {
+    fontSize: 16,
+    padding: 8
+}
+const SKILL_PICKER = StyleSheet.create({
+    inputIOS: {
+      fontSize: 16,
+      paddingVertical: 12,
+      paddingHorizontal: 10,
+      borderWidth: 1,
+      borderColor: 'gray',
+      borderRadius: 4,
+      color: 'black',
+      paddingRight: 30, // to ensure the text is never behind the icon
+    },
+    inputAndroid: {
+      fontSize: 16,
+      paddingHorizontal: 10,
+      paddingVertical: 8,
+      borderWidth: 0.5,
+      borderColor: 'purple',
+      borderRadius: 8,
+      color: 'black',
+      paddingRight: 30, // to ensure the text is never behind the icon
+    },
+  });
+
+// SKILLS is the object of the users experience in each life skill
+const SKILLS = [
+    {
+        value: 0,
+        label: "Cleaning",
+        experience: 620
+    },
+    {
+        value: 1,
+        label: "Cooking",
+        experience: 100
+    },
+    {
+        value: 2,
+        label: "Mindfulness",
+        experience: 350
+    },
+    {
+        value: 3,
+        label: "Hygiene",
+        experience: 700
+    }
+]
+
+
 
 let experienceCalculator = function(time: number): number {
     return time * 100;
@@ -47,10 +99,17 @@ export const AddScreen: Component = observer(function AddScreen() {
 
             <View> 
                 <Text style={TEXT}>How long did it take you to complete?</Text>
+                <TextInput style={INPUT} placeholder="5..." />
+                <Text style={TEXT}>Minutes</Text>
             </View>
 
             <View>  
                 <Text style={TEXT}>What skill does this activity fall under?</Text>
+                <RNPickerSelect 
+                    style={SKILL_PICKER}
+                    onValueChange={(value) => console.log(value)}
+                    items={SKILLS}
+                />
             </View>
 
             <View testID="ExperienceCalculator" style={{flexDirection: "row"}}>
@@ -58,7 +117,10 @@ export const AddScreen: Component = observer(function AddScreen() {
                 <Text style={BOLD}> {experienceCalculator(activityTime)}</Text>
             </View> 
 
-            <Button></Button>
+            <View style={ADD_BUTTON}>
+                <Button text="Add Activity" textStyle={ADD_BUTTON}/>
+            </View>
+            
         </View>
     );
 });
